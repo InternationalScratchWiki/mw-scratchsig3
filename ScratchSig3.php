a99d207176916ed5018c152ea8bea2c41995a84b
Naleksuh <naleksuh@gmail.com>
2020-10-14 18:02:57 -0700

    Update ScratchSig3.php

--- ScratchSig3.php
+++ ScratchSig3.php
@@ -5,10 +5,10 @@ class ScratchSig {
 	}
 
 
-public static function onscratchsig ($input, array $args, Parser $parser, PPFrame $frame) {
-	if (!isset($GLOBALS["sigimageurl"][$input])) {
-		$GLOBALS["sigimageurl"][$input] = "http://cdn.scratch.mit.edu/get_image/user/" . json_decode(file_get_contents("https://api.scratch.mit.edu/users/$input"), $assoc=true)["id"] . "_18x18.png";
-	}
-	return "<br/><img src=\"" . $GLOBALS["sigimageurl"][$input] . "\" width=\"18px\" height=\"18px\"> " . $parser->recursiveTagParse("[[User:$input|$input]] ([[User_talk:$input#top|talk]] {{!}} [[Special:Contributions/$input|contribs]])");
+	public static function onscratchsig ($input, array $args, Parser $parser, PPFrame $frame) {
+		if (!isset($GLOBALS["sigimageurl"][$input])) {
+			$GLOBALS["sigimageurl"][$input] = "http://cdn.scratch.mit.edu/get_image/user/" . json_decode(file_get_contents("https://api.scratch.mit.edu/users/$input"), $assoc=true)["id"] . "_18x18.png";
+		}
+		return "<br/><img src=\"" . $GLOBALS["sigimageurl"][$input] . "\" width=\"18px\" height=\"18px\"> " . $parser->recursiveTagParse("[[User:$input|$input]] ([[User_talk:$input#top|talk]] {{!}} [[Special:Contributions/$input|contribs]])");
 	}
 }
