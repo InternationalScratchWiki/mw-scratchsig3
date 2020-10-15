7c4216970f69caf9fb0fcb59725ea78d9cd694f1
Naleksuh <naleksuh@gmail.com>
2020-10-14 18:01:45 -0700

    Update ScratchSig3.php

--- ScratchSig3.php
+++ ScratchSig3.php
@@ -9,7 +9,6 @@ public static function onscratchsig ($input, array $args, Parser $parser, PPFram
 	if (!isset($GLOBALS["sigimageurl"][$input])) {
 		$GLOBALS["sigimageurl"][$input] = "http://cdn.scratch.mit.edu/get_image/user/" . json_decode(file_get_contents("https://api.scratch.mit.edu/users/$input"), $assoc=true)["id"] . "_18x18.png";
 	}
-	// why must avatars ruin signatures :(
 	return "<br/><img src=\"" . $GLOBALS["sigimageurl"][$input] . "\" width=\"18px\" height=\"18px\"> " . $parser->recursiveTagParse("[[User:$input|$input]] ([[User_talk:$input#top|talk]] {{!}} [[Special:Contributions/$input|contribs]])");
 	}
 }
